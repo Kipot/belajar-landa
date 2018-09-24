6b3daece4a7a952f42c3e0ffa8988f8a05d5aaca
naufalnibros <masnaufal05@gmail.com>
2018-09-24 17:08:01 +0700

    push pertama

---
+++ index.php
@@ -0,0 +1,78 @@
+<?php
+
+// echo "SELAMAT DATANG DI LANDA SYSTEMS";
+
+// Array (Multidimensi, Assosiative)
+
+// FUCNTION PHP
+// empty()
+// isset()
+// unset()
+// unlink()
+// is_array()
+
+// Array
+// $siswa[0] = "naufal";
+// $siswa[1] = "naufal";
+// $siswa[2] = "yusron";
+// $siswa[3] = "dll";
+
+// $siswa = ["naufal", "yusrom", "dll"];
+// var_dump($siswa);
+
+// Array Assosiative
+$siswa["nama"]  = "NAUFAL";
+$siswa["nama"]  = "YUSRON";
+
+$siswa["kelas"] = "A 12";
+$siswa["kelas"] = "B 12";
+
+
+// Array Assosiative & Multidimensi
+// $siswa["nama"][0]  = "NAUFAL";
+// $siswa["nama"][1]  = "YUSRON";
+//
+// $siswa["kelas"][0] = "A 12";
+// $siswa["kelas"][1] = "B 12";
+
+// var_dump();
+// print_r($siswa);
+
+// ================== EMPTY() =============================
+// $siswa = "NAUFAL";
+// if (empty($siswa)) {
+//   echo "TIDAK ADA ISI";
+// } else {
+//   echo "ADA ISI";
+// }
+
+// ================== ISSET() =============================
+// $siswa = NULL;
+// $siswa = "";
+// $siswa = array();
+
+// if (isset($siswa)) {
+//   echo "ADA DATA";
+// } else {
+//   echo "TIDAK ADA DATA";
+// }
+
+// ================== UNSET() =============================
+// $siswa = array(
+//   'kelas' => "A12",
+//   'nama'  => "Rahmad");
+// unset($siswa['nama']);
+// var_dump($siswa);
+
+// ================== UNLINK() =============================
+// $file = "rahmad.txt";
+// unlink($file);
+
+// ================== IS_ARRAY() =============================
+// if (is_array($siswa)) {
+//   echo "INI ARRAY";
+// } else {
+//   echo "INI BUKAN ARRAY";
+// }
+
+ ?>
