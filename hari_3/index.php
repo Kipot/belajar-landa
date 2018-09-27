d8e8bc1da3d5ff87b8c3a55d0cb1bfbb3b8d4c6d
naufalnibros <masnaufal05@gmail.com>
2018-09-27 18:09:53 +0700

    push hari-3

---
+++ hari_3/index.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+    <link rel="stylesheet" href="style.css">
+  </head>
+  <body>
+
+    <!-- <div class="container">
+      <div class="jumbotron">
+        <h1>Bootstrap Tutorial</h1>
+        <p>
+          Bootstrap is the most popular HTML, CSS, and JS framework for developing
+          responsive, mobile-first projects on the web.
+        </p>
+      </div>
+      <p>This is some text.</p>
+      <p>This is another text.</p>
+    </div>
+
+    <div class="container">
+      <button type="button" class="btn btn-primary">
+        SUBMIT
+      </button>
+    </div> -->
+
+    <div class="container">
+      <h1>Grid</h1>
+      <p>This example demonstrates a 50%/50% split on small, medium and large devices. On extra small devices, it will stack (100% width).</p>
+      <p>Resize the browser window to see the effect.</p>
+      <div class="row">
+        <div class="col-sm-4 col-xs-6" style="background-color:blue;">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.<br>
+          Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+        </div>
+        <div class="col-sm-4 col-xs-6" style="background-color:yellow;">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.<br>
+          Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+        </div>
+        <div class="col-sm-4 col-xs-12" style="background-color:pink;">
+          Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto.
+        </div>
+      </div>
+    </div>
+
+    <!-- <div class="container">
+      <h1>Grid</h1>
+      <p>This example demonstrates a 50%/50% split on small, medium and large devices. On extra small devices, it will stack (100% width).</p>
+      <p>Resize the browser window to see the effect.</p>
+      <div class="row">
+        <div class="standart kolom-6">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.<br>
+          Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+        </div>
+        <div class="standart kolom-6">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.<br>
+          Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+        </div>
+        <div class="standart kolom-12">
+          Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto.
+        </div>
+      </div>
+    </div> -->
+
+  </body>
+  <!-- Optional -->
+
+  <!-- jQuery -->
+  <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script> -->
+  <!-- Minified JavaScript -->
+  <!-- <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script> -->
+
+</html>
---
+++ methode.php
@@ -0,0 +1,17 @@
+<?php echo $_POST['angka']; ?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <form class="" action="" method="post">
+      <input type="text" name="angka" value="">
+      <button type="submit" name="button" value="submit">
+        Submit Bro!
+      </button>
+    </form>
+  </body>
+</html>
