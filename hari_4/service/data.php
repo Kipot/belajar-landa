6e32c3b7271e4b85b6019080ca04fea8bbdb54fb
naufalnibros <masnaufal05@gmail.com>
2018-09-28 19:38:42 +0700

    hari 4

---
+++ hari_4/service/data.php
@@ -0,0 +1,16 @@
+<?php
+
+// $siswa[0] = "NAUFAL";
+// $siswa[1] = "YUSRON";
+// $siswa[2] = "RAHMAD";
+// $siswa[3] = "TOPIK";
+
+$siswa = (object)[
+  'nama'  => "naufal",
+  'kelas' => "12A"
+];
+
+
+echo json_encode($siswa);
+
+ ?>
